8bcc652930e370e8e87d46bb525e84fee02005da
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2023-12-29 13:39:32 +0100

    Add hikey960 target.

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 cd u-boot
 export ROCKCHIP_TPL="$(ls ../rkbin/bin/rk35/rk3566_ddr_1056MHz_v*.bin | sort | tail -n1)"
 export BL31="$(ls ../rkbin/bin/rk35/rk3568_bl31_v*.elf | sort | tail -n1)"
-#git apply ../patches/soquartz/*.patch
 make soquartz-cm4-rk3566_defconfig
 make -j$(nproc)
 mkdir -p ../bin/soquartz
@@ -22,6 +21,24 @@ make p3450-0000_defconfig
 make -j$(nproc)
 mkdir -p ../bin/jetson-nano
 cp u-boot.bin ../bin/jetson-nano/
+git reset --hard
+git clean -f -d
+cd ..
+
+# build hikey960
+cd u-boot
+make hikey960_defconfig
+make -j$(nproc)
+mkdir -p ../bin/hikey960
+cp u-boot.bin ../bin/hikey960/
+git reset --hard
+git clean -f -d
+cd ..
+
+cd arm-trusted-firmware
+make all fip PLAT=hikey960 BL33=../bin/hikey960/u-boot.bin SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey960/lpm3.img
+cp build/hikey960/release/bl2.bin ../bin/hikey960/l-loader.bin
+cp build/hikey960/release/fip.bin ../bin/hikey960/
 git reset --hard
 git clean -f -d
 cd ..
